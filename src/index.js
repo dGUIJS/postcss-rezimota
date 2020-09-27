16102814d64666a59a7e9d25e653912f33552571
Comandeer <vepomoc@gmail.com>
2020-09-27 17:35:34 +0200

    feat(index): add support for at-rule syntax

--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
+import { Declaration, Rule } from 'postcss';
 import aliases from './aliases.js';
 
 function plugin() {
 	return {
 		postcssPlugin: 'postcss-rezimota',
+
 		Declaration( declaration ) {
-			if ( !aliases.has( declaration.prop ) ) {
+			const alias = getAlias( declaration.prop, aliases );
+
+			if ( !alias ) {
 				return;
 			}
 
-			declaration.prop = aliases.get( declaration.prop );
+			declaration.prop = alias;
+		},
+
+		AtRule( atRule ) {
+			if ( !( atRule.parent instanceof Rule ) ) {
+				return;
+			}
+
+			const alias = getAlias( atRule.name, aliases );
+
+			if ( !alias ) {
+				return;
+			}
+
+			const value = parseParams( atRule.params );
+			const declaration = new Declaration( {
+				prop: alias,
+				value
+			} );
+
+			atRule.replaceWith( declaration );
 		}
 	};
 }
 
 plugin.postcss = true;
+
+function getAlias( name, aliases ) {
+	name = name.toLowerCase();
+
+	return aliases.get( name );
+}
+
+function parseParams( params ) {
+	return params.replace( /[()]/g, '' ).trim();
+}
 
 export default plugin;
