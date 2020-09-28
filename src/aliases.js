fa79c1a7adb594b097a322fb49c82d9722931391
Comandeer <vepomoc@gmail.com>
2020-09-29 00:01:28 +0200

    feat(aliases): add aliases from anim to bdx

--- src/aliases.js
+++ src/aliases.js
@@ -1,4 +1,16 @@
 const aliases = new Map( [
+	[ 'anim', 'animation' ],
+	[ 'animdel', 'animation-delay' ],
+	[ 'animdir', 'animation-direction' ],
+	[ 'animdur', 'animation-duration' ],
+	[ 'animfm', 'animation-fill-mode' ],
+	[ 'animic', 'animattion-iteration-count' ],
+	[ 'animn', 'animation-name' ],
+	[ 'animps', 'animation-play-state' ],
+	[ 'animtr', 'animation-timing-function' ],
+	[ 'ap', 'appearance' ],
+	[ 'bd', 'border' ],
+	[ 'bdx', [ 'border-left', 'border-right' ] ],
 	[ 'bgc', 'background-color' ]
 ] );
 
